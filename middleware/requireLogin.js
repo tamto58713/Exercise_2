99509565636fcb5e4f9eb7db2806deac9fe8880b
Tam To Tran <tamto@localhost.localdomain>
2019-06-25 10:28:18 +0700

    fix login for orders

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -3,7 +3,7 @@ module.exports.requireLogin = (req, res, next) => {
     if (!req.cookies.userId)
     {
 
-        res.redirect('auth/login');
+        res.redirect('/auth/login');
         return;
     }
     // if (!res.cookies.userId)
